70f9f5ba4af91d1cbafcc54d5a5e1dbf6b73715e
student <student@localhost>
2019-09-18 21:04:58 +0300

    fixes #5, fixes #6

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double volume;
-        double fuelUsage;
 
         System.out.print("Введите объем бака Вашего автомобиля в литрах: ");
-        volume = input.nextDouble();
+        int volume = (int) input.nextInt();
         System.out.println("Объема Вашего бака: " + volume + " л.");
 
         System.out.print("Введите расход топлива Вашего автомобиля, литров на 100 км.: ");
-        fuelUsage = input.nextDouble();
+        double fuelUsage = input.nextDouble();
         System.out.println("Расход топлива Вашего автомобиля: " + fuelUsage + " л./100 км.");
 
         double distance = (volume / fuelUsage) * 100;
